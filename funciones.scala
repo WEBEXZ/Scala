59be729cc689f19749056d9c516bda988a1a40af
WEBEXZ <audi.icon@gmail.com>
2016-07-13 18:29:31 -0500

    Objeto Singleton

---
+++ funciones.scala
@@ -0,0 +1,11 @@
+object Persona {
+  def saludar(nombre: String): Unit = {
+    println("Hola " + nombre)
+  }
+  def mostrarSuma(x: Int): Unit = {
+    println(x)
+  }
+  def sumarEnteros(x: Int, y: Int): Int = {
+    x + y
+  }
+}
