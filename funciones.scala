697b05366f62c7bd4ae0faef2cbe59fff78061a5
WEBEXZ <audi.icon@gmail.com>
2016-07-13 18:57:11 -0500

    Utilizando match

--- funciones.scala
+++ funciones.scala
@@ -8,4 +8,22 @@ object Persona {
   def sumarEnteros(x: Int, y: Int): Int = {
     x + y
   }
+  def decirDia(d: String): Unit = {
+    println(d match {
+      case "l" => "Lunes"
+      case "m" => "Martes"
+      case "mi" => "Miercoles"
+      case "j" => "Jueves"
+      case "v" => "Viernes"
+      case "s" => "Sabado"
+      case "d" => "Domingo"
+      case _ => "No existe"
+    })
+  }
 }
+
+//Evaluaciones
+Persona.saludar("Webexz!")
+val resultado = Persona.sumarEnteros(5, 10)
+Persona.mostrarSuma(resultado)
+Persona.decirDia("r")
